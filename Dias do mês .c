cee828e16ce3b00bf7e428382ef872957ef4b442
XChrisZero <161791400+XChrisZero@users.noreply.github.com>
2024-03-25 21:55:25 -0300

    Dias do mês .c
    Escreva um programa que indique o número de dias existentes em um mês

---
+++ Dias do mês .c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+
+int main()
+{
+int mes;
+
+printf ("qual mes: \n");
+scanf("%i", &mes);
+switch (mes)
+{
+case 02:
+printf ("28 dias");
+break;
+
+case 04:
+printf ("30 dias");
+break;
+
+case 06:
+printf ("30 dias");
+break;
+
+case 9:
+printf ("30 dias");
+
+break;
+
+case 11:
+printf ("30 dias");
+break;
+
+case 01:
+printf ("31 dias");
+break;
+
+case 03:
+printf ("31 dias");
+break;
+
+case 05:
+printf ("31 dias");
+break;
+
+case 07:
+printf ("31 dias");
+break;
+
+case 8:
+printf ("31 dias");
+break;
+
+case 10:
+printf ("31 dias");
+break;
+
+case 12:
+printf ("31 dias");
+break;
+
+default:
+printf("mes invalido");
+break;
+}
+
+}#include <stdio.h>
+
+int main()
+{
+int mes;
+
+printf ("qual mes: \n");
+scanf("%i", &mes);
+switch (mes)
+{
+case 02:
+printf ("28 dias");
+break;
+
+case 04:
+printf ("30 dias");
+break;
+
+case 06:
+printf ("30 dias");
+break;
+
+case 9:
+printf ("30 dias");
+
+break;
+
+case 11:
+printf ("30 dias");
+break;
+
+case 01:
+printf ("31 dias");
+break;
+
+case 03:
+printf ("31 dias");
+break;
+
+case 05:
+printf ("31 dias");
+break;
+
+case 07:
+printf ("31 dias");
+break;
+
+case 8:
+printf ("31 dias");
+break;
+
+case 10:
+printf ("31 dias");
+break;
+
+case 12:
+printf ("31 dias");
+break;
+
+default:
+printf("mes invalido");
+break;
+}
+
+}
