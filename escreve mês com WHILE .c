e546741595b4aa018a18023710dd449e3894d076
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-04-04 00:51:39 -0300

    WHILE .c
    Criar um programa que imprima o número e o nome do mês
    Exemplo de saída:
    1-	janeiro
    2-	fevereiro
    3-	março …
    12- dezembro

---
+++ escreve mês com WHILE .c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int cont = 1;
+   while(cont >=1 && cont<= 12) {
+       
+    printf("%i - Janeiro \n", cont);
+     cont++;
+    printf("%i - Fevereiro \n", cont);
+     cont++;
+    printf("%i - Marco \n", cont);
+     cont++;
+    printf("%i - Abril \n", cont);
+     cont++;
+    printf("%i - Maio \n", cont);
+     cont++;
+    printf("%i - Junho \n", cont);
+     cont++;
+    printf("%i - Julho \n", cont);
+     cont++;
+    printf("%i - Agosto \n", cont);
+     cont++;
+    printf("%i - Setembro \n", cont);
+     cont++;
+    printf("%i - Outubro \n", cont);
+     cont++;
+    printf("%i - Novembro \n", cont);
+     cont++;
+    printf("%i - Dezembro \n", cont);
+     cont++;
+   
+   }
+}
