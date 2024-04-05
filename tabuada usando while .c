9c4dfb5fb9a4a592c01804e8c113429611722ccb
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-04-05 19:20:12 -0300

    Atualizar o tabuada usando while .c

--- tabuada usando while .c
+++ tabuada usando while .c
@@ -2,22 +2,22 @@
 #include <stdbool.h>  /*biblioteca booleana foi incluida pq */
 int main()
 {
-    bool controle = false; /**/
+    bool controle = false; /* quando o controle terno mesmo valor q false Vai entrar entrar no WHILE*/
     int n1;
     
-    while(controle == false) /* esse while(enquanto) ==false é pq*/
+    while(controle == false) /* esse while(enquanto) ==false é Para ele rodar até ele for verdadeiro, no caso enquanto ele for false ele vai rodar */
     {
         printf("Digite um numero entre 1 e 10: ");
         scanf("%i", &n1);
         
         if (n1 >= 1 && n1 <= 10) /* && representa E, and. */
         {
-            int contador;  /* */
+            int contador;  /* para o segundo while contar ate <=10*/
             while(contador <= 10)  /* ira rodar até 10 vezes*/
             {
-                printf("%i X %i = %i \n", n1, contador, n1 * contador);  /* */
+                printf("%i X %i = %i \n", n1, contador, n1 * contador);  /* tudo depois das aspas é o que está explicando os %i's*/
                 contador++;  /*a atribuição ++ foi feita aqui para o while(enquanto) adicionar 1 a cada vez q rodar*/
-                controle = true;  /* */
+                controle = true;  /* para encerrar quando o contador <=10 do while de cima */
             }
             
         }
