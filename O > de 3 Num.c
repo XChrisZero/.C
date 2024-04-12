2eb986982a62c901671c184b0ec78f3a75241f97
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-04-11 21:46:19 -0300

    Exercício 2 (b1)
    2 Escreva um programa que receba 3 valores e informe qual é o maior

---
+++ O > de 3 Num.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+float n1, n2, n3;
+int contador = 1;
+
+printf (" Digite um numero: \n");
+scanf ( "%f",&n1 );
+
+printf (" Digite um numero: \n");
+scanf ( "%f",&n2 );
+
+printf (" Digite um numero: \n");
+scanf ( "%f",&n3 );
+
+    if(n1 > n2 && n1 > n3){
+printf("O numero maior é: %.0f", n1);
+}
+    else if(n2 > n1 && n2 > n3){
+printf("O numero maior é: %.0f", n2);
+}
+    else {
+printf ("O numero maior é: %.0f", n3);
+}
+}
