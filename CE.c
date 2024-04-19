8de406596d62b86a8144ee97082782ae908f886f
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-04-19 11:49:45 -0300

    INACABADO
    Escreva um programa que simule um caixa eletrônico.
    O programa encerra somente quando o usuário digitar a opção 4 (Sair).

---
+++ CE.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+        printf ( "\n--------------------Menu Principal:--------------------\n\n" );
+        printf ( "\n    1- Consulta.\n" );
+        printf ( "\n    2- Saque.\n" );
+        printf ( "\n    3- Depósito.\n" );
+        printf ( "\n    4. Sair." );
+        
+         printf ( "\n    <- voltar." );
+
+
+    return 0;
+}
