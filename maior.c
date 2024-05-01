62508926cd7aab0fd812363b144b6cfa5dc3fa31
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-05-01 15:38:46 -0300

    If Elseif Else
    Diferença entre dois numeros

--- maior.c
+++ maior.c
@@ -11,15 +11,15 @@ int main()
     printf( "Digite segundo numero\n" );
     scanf( "%f", &num2 );
     
-    if(num1 > num2){ 
-        printf("Numero maior é: %.2f",num1);
+    if(num1 > num2)     { 
+     printf("Numero maior é: %.2f",num1);
         
     }
-    else if( num1 == num2 ){
-        printf("Numeros iguais");
+    else if( num1 == num2 )     {
+     printf("iguais");
     }
-    else{
-        printf("Numero maior é: %.2f", num2);
+    else    {
+     printf("Numero maior é: %.2f", num2);
     }
 
 }
