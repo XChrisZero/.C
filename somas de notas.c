291c73a34a2d05e4f8ca676dbfa4b6464897be26
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-05-01 15:32:24 -0300

    If Elseif Else
    Soma de duas notas de 0 a 10

--- somas de notas.c
+++ somas de notas.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void)  {
   int nota1, nota2, media;
   
-
-  printf("Digite a primeira nota do aluno: ");
-  scanf("%i",&nota1);
+    printf("Digite a primeira nota do aluno(0 a 10): ");
+    scanf("%i",&nota1);
   
-  printf("Digite a segunda nota do aluno: ");
-  scanf("%i",&nota2);
+    printf("Digite a segunda nota do aluno(0 a 10): ");
+    scanf("%i",&nota2);
   
-  media = (nota1 + nota2) / 2;
+        media = (nota1 + nota2) / 2;
   
-  printf("Media do aluno = %i\n",media);
+    printf("Media do aluno = %i\n",media);
   
-  if(media >= 6){
-      printf("Aprovado!");
+   if(media >= 6)   {
+     printf("Aprovado!");
       
   }
-  else if(media >= 4 && media <= 5){
+   else if(media >= 4 && media <= 5)    {
       printf("Exame!");
       
+   
   }
-  else{
+   else  {
       printf("Reprovado!");
-  }
+}
 }
