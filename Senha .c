16a8a153e49c600e595bfeea1852c08e1afb9fb0
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-05-08 22:41:11 -0300

    While, IF
    Faça um programa que receba uma senha formada de quatro números inteiros,
    verifique se a senha está correta e, caso não esteja, solicite novamente a senha.
    Se a senha digitada estiver correta, deverá ser apresentada a mensagem
    “Senha Correta”, caso contrário, “Senha Incorreta”.

--- Senha .c
+++ Senha .c
@@ -7,20 +7,25 @@ int main()
     
     while ( new == password ) /* "==" usado para verificar se dois valores são iguais */
     {
-        printf("Digite a sua nova senha: \n");
+        printf("\n  Digite a sua nova senha: \n  ");
         scanf("%i", &new );
-        printf("Digite sua senha: \n");
+        printf("  Digite sua senha: \n  ");
         scanf("%i", &password);
+        
         
         if( new != password ) /* "!=" o operador usado para verificar se dois valores são diferentes */
         {
-            printf("Senha Incorreta! \n");
+            printf("        ----------------\n");
+            printf("        Senha Incorreta! \n");
+            printf("        ----------------\n");
            
         }
         
         else /* se sua senha for igual a senha antiga*/
         {
-            printf("Senha Correta! \n");
+            printf("        --------------\n");
+            printf("        Senha Correta! \n");
+            printf("        --------------\n");
         }
         
        
