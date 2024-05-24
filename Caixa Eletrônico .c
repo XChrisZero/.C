57b9bc986374bdbe14e06deedce3809081f1e0da
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-05-23 22:47:35 -0300

    Obra de arte
    Escreva um programa que simule um caixa eletrônico.
    
    - O valor inicial do saldo é zero.
    - Quando o usuário digitar a opção 1, devemos imprimir o valor do saldo
    - Quando o usuário digitar a opção 2, devemos permitir que o usuário digite o valor
    de saque.
    - Quando o usuário digitar a opção 3, devemos permitir que o usuário digite o valor
    de depósito.
    - A cada operação de saque devemos subtrair o valor digitado pelo usuário do valor
    do saldo.
    - A cada operação de depósito devemos somar o valor digitado pelo usuário do valor
    do saldo.
    - O programa encerra quando o usuário digitar a opção 4 (Sair)

--- Caixa Eletrônico .c
+++ Caixa Eletrônico .c
@@ -1,70 +1,77 @@
 #include <stdio.h>
 #include <stdbool.h>
-int main()  {
+int main()  
+{
 
-bool bitcoin = true;
-int op; /*Opcões de menu, e uma para o laço*/
-float vs, vd; /* valor do saque e valor de deposito*/
-float sc = 0.0; /* Saldo em conta*/
+ bool bitcoin = true;
+ int op; /*Opcões de menu*/
+ float vs, vd; /* valor do saque e valor de deposito*/
+ float sc = 0.0; /* Saldo em conta*/
 
-    while (bitcoin == true )    {
-        printf ( "\n--------------------Menu Principal:--------------------\n\n" );
-        printf ( "\n            <- Digite uma opção -> \n" );
-        printf ( "\n            1- Consulta de saldo.\n" );
-        printf ( "\n            2- Saque alguem valor.\n" );
-        printf ( "\n            3- Depósitar algum valor.\n" );
-        printf ( "\n            4. Encerrar transação.\n" );
-        scanf ( "%i", &op );
-        printf ( "\n-------------------------------------------------------\n" );
-
-switch (op)    {
-    case 1: /* Consultar o saldo*/
-        printf ( "\n                Seu Saldo atual é:R$ %.2f \n ", sc );
-        printf ( "\n-------------------------------------------------------\n" );
-        break;
-    
-    case 2: /* Sacar algum saldo*/
-        printf ( "\n                Digite o valor do saque: R$\n" );
-        scanf ( "%f", &vs );
-        printf ( "\n-------------------------------------------------------\n" );
-        
-        if (vs <= sc)    {
-            sc = sc - vs; /* saldo em conta -= valor do saque*/
-            printf ( "\n                Saque feito com sucesso!\n" );
-            printf ( "\n                Saldo em conta é R$%.2f\n", sc );
-            printf ( "\n-------------------------------------------------------\n" );
+    while (bitcoin == true ) 
+    {
+     printf ( "\n--------------------Menu Principal:--------------------\n\n" );
+     printf ( "\n            <- Digite uma opção -> \n" );
+     printf ( "\n            1- Consulta de saldo.\n" );
+     printf ( "\n            2- Saque alguem valor.\n" );
+     printf ( "\n            3- Depósitar algum valor.\n" );
+     printf ( "\n            4. Encerrar transação.\n" );
+     scanf ( "%i", &op );
+     printf ( "\n-------------------------------------------------------\n" );
+       
+        switch (op)    
+        {
+         
+            case 1: /* Consultar o saldo*/
+             printf ( "\n                Seu Saldo atual é:R$ %.2f \n ", sc );
+             printf ( "\n-------------------------------------------------------\n" );
+             break;
+         
+            case 2: /* Sacar algum saldo*/
+             printf ( "\n                Digite o valor do saque: R$\n" );
+             scanf ( "%f", &vs );
+             printf ( "\n-------------------------------------------------------\n" );
+             
+                if (vs <= sc)    
+                {
+                 sc = sc - vs; /* saldo em conta -= valor do saque*/
+                 printf ( "\n                Saque feito com sucesso!\n" );
+                 printf ( "\n                Saldo em conta é R$%.2f\n", sc );
+                  printf ( "\n-------------------------------------------------------\n" );
+                }
+             
+                else   
+                {
+                 printf ( "\n                Quantidade insuficiente para saque! \n" );
+                  printf ( "\n                você possui R$%.2f na conta\n", sc );
+                 printf ( "\n-------------------------------------------------------\n" );
+                }
+             break;
+         
+            case 3: /*Depositar algum saldo*/
+             printf ( "\n                Quanto deseja depositar?: R$" );
+             scanf("%f", &vd);
+             printf ( "\n-------------------------------------------------------\n" );
+             
+             sc = sc + vd; /* saldo em conta += valor do deposito*/ 
+             
+             printf ( "\n                Depósito realizado com sucesso!\n" );
+             printf ( "\n                Saldo em conta é: R$%.2f\n", sc );
+             printf ( "\n-------------------------------------------------------\n" );
+             break;
+         
+             case 4: /*Encerrar a transação*/
+             printf ( "\n                Volte Sempre!\n" );
+             printf ( "\n-------------------------------------------------------\n" );
+             bitcoin = false;
+             break;
+         
+         default: /*Mensagem de erro*/
+         printf ( "\n     Opção inválida!!!. \n" );
+         printf ( "\n    Digite um dos seguintes numeros 1,2,3 ou 4.\n" );
+         printf ( "\n-------------------------------------------------------\n" );
         }
-        else    {
-            printf ( "\n                Quantidade insuficiente para saque! \n" );
-            printf ( "\n                você possui R$%.2f na conta\n", sc );
-            printf ( "\n-------------------------------------------------------\n" );
-        }
-        break;
-   
-    case 3: /*Depositar algum saldo*/
-        printf ( "\n                Quanto deseja depositar?: R$" );
-        scanf("%f", &vd);
-        printf ( "\n-------------------------------------------------------\n" );
-        
-        sc = sc + vd; /* saldo em conta += valor do deposito*/ 
-        
-        printf ( "\n                Depósito realizado com sucesso!\n" );
-        printf ( "\n                Saldo em conta é: R$%.2f\n", sc );
-        printf ( "\n-------------------------------------------------------\n" );
-        break;
-
-    case 4: /*Encerrar a transação*/
-        printf ( "\n                Volte Sempre!\n" );
-        printf ( "\n-------------------------------------------------------\n" );
-        bitcoin = false;
-        break;
-
-    default: /*Mensagem de erro*/
-        printf ( "\n     Opção inválida!!!. \n");
-        printf ( "\n    Digite um dos seguintes numeros 1,2,3 ou 4.\n" );
-        printf ( "\n-------------------------------------------------------\n" );
-}
-
+     
     }
     
-}
+} 
